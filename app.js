700003661ac6ff358d526ce846b32400e11b4da2
Aadhitya Kumar <aadhitya.k22@gmail.com>
2022-12-16 20:28:11 +0530

    Modified urls

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 document
   .getElementsByClassName("img1")[0]
-  .setAttribute("src", "/images/dice" + randomNumber1 + ".png");
+  .setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document
   .getElementsByClassName("img2")[0]
-  .setAttribute("src", "/images/dice" + randomNumber2 + ".png");
+  .setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩Player 1 wins";
